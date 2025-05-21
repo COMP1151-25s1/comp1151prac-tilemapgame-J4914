e67159f4d3c5bfa23f407e186afdb7c69f952af1
J4914 <jonah.grippi@students.mq.edu.au>
2025-05-21 18:41:23 +1000

    Commit 2

--- Assets/Tiles/TileMapGame/TinyTown2.tsx
+++ Assets/Tiles/TileMapGame/TinyTown2.tsx
@@ -1,4 +1,83 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="TinyTown2" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="tilemap_packed.png" width="192" height="176"/>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="2" width="12" height="14"/>
+   <object id="2" x="4" y="2" width="12" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="12" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="2" width="8" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="12" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="12" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="2" width="12" height="12"/>
+   <object id="2" x="4" y="2" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="5" width="16" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="12" height="12"/>
+   <object id="2" x="0" y="2" width="12" height="12"/>
+   <object id="3" x="0" y="2" width="12" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="14"/>
+  </objectgroup>
+ </tile>
 </tileset>
